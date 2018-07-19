6cf35442f3f8ca6fd4ea4158c1d9de7bc03a58ba
Irene Lee <irenelee914@gmail.com>
2018-07-19 17:45:32 -0400

    Initial Commit

---
+++ Study Planner/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Study Planner
+//
+//  Created by Hojoon Lee on 2018-07-19.
+//  Copyright © 2018 Irene Lee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
